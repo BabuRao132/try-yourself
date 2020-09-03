1b59f8e3b8ab4718aae7db7001cfb3c56696381a
BabuRao132 <70302147+BabuRao132@users.noreply.github.com>
2020-09-03 22:16:44 +0530

    Create Try_yourself.py

---
+++ Try_yourself.py
@@ -0,0 +1,38 @@
+#1
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  
+#2
+i=1
+while i<6:
+  print(i)
+    i+=1
+   #3
+a=50
+b=10
+if a>b:
+  print("Hello world")
+
+#4
+x=input("enter a number")
+x=int(x)
+if x%2==0:
+  print('%s is even'%(x))
+  else:
+    print('%s is odd'%(x))
+ #5
+ y=int(input("enter marks"))
+ if y>90:
+   print("GRADE A")
+   elif y>60 and y<=90:
+     print("GRADE B")
+     else:
+       print("GRADE F")
+    #6
+    for x in range(10,51):
+      print(x)
+   #7
+   for x in range(10,51):
+     if x%2!=0:
+         print(x)
